e6bbfbd2bba128a6bb35d923e05b7af666769184
Tiago Castilho <tiagocastilho_bm@hotmail.com>
2022-03-04 23:36:06 -0300

    Iniciando configuração do JS

---
+++ Projeto 8 - Urna Eletrônica/script.js
@@ -0,0 +1,6 @@
+let seuVotoPara = document.querySelector('left1 span');
+let cargo = document.querySelector('left2 span');
+let descricao = document.querySelector('left4');
+let aviso = document.querySelector('divisao2');
+let lateral = document.querySelector('divisao1-right');
+let numeros = document.querySelector('left3');
