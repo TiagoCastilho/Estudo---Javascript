c22ca579e5e6291a214e2a57215e99a12d9e982a
Tiago Castilho <tiagocastilho_bm@hotmail.com>
2022-03-07 17:00:40 -0300

    Projeto finalizado com urna funcional

--- Projeto 8 - Urna Eletrônica/etapas.js
+++ Projeto 8 - Urna Eletrônica/etapas.js
@@ -4,21 +4,70 @@ let etapas = [
         numeros: 5,
         candidatos: [
             {
-                numero: 38111,
-                nome: 'Fulano de Tal',
+                numero: '11111',
+                nome: 'Stephanie Quintal',
                 partido: 'ABC',
                 fotos: [
                     {url: '1-1.png', legenda: 'Vereador', small: false}
                 ]
             },
             {
-                numero: 77222,
-                nome: 'Beltrano da Silva',
-                partido: 'DEFG',
+                numero: '22222',
+                nome: 'Oséias Pinto',
+                partido: 'DEF',
+                fotos: [
+                    {url: '1-2.png',legenda: 'Vereador',small: false}
+                ]
+            },
+            {
+                numero: '33333',
+                nome: 'Olivia Paulos',
+                partido: 'GHI',
+                fotos: [
+                    {url: '2-1.png',legenda: 'Vereador',small: false}
+                ]
+            },
+            {
+                numero: '44444',
+                nome: 'Luena Rebimbas',
+                partido: 'ABC',
+                fotos: [
+                    {url: '2-2.png',legenda: 'Vereador',small: false}
+                ]
+            },
+            {
+                numero: '55555',
+                nome: 'Filomena Mexia',
+                partido: 'DEF',
+                fotos: [
+                    {url: '3-1.png',legenda: 'Vereador',small: false}
+                ]
+            },
+            {
+                numero: '66666',
+                nome: 'Kelson Tigre',
+                partido: 'GHI',
+                fotos: [
+                    {url: '3-2.png',legenda: 'Vereador',small: false}
+                ]
+            },
+            {
+                numero: '77777',
+                nome: 'Mickael Feitosa',
+                partido: 'ABC',
+                fotos: [
+                    {url: '4-2.png',legenda: 'Vereador',small: false}
+                ]
+            },
+            {
+                numero: '88888',
+                nome: 'Elisabete Pinto',
+                partido: 'DEF',
                 fotos: [
                     {url: '2-1.png',legenda: 'Vereador',small: false}
                 ]
             }
+            
         ]
     },
     {
@@ -26,23 +75,43 @@ let etapas = [
         numeros: 2,
         candidatos: [
             {
-                numero: 99,
-                nome: 'Ciclano',
+                numero: '11',
+                nome: 'Ayla Castelo',
                 partido: 'ABC',
-                vice: 'Cic',
+                vice: 'Isabel Parafita',
                 fotos: [
-                    {url: '3-1.png', legenda: 'Prefeito', small: false},
-                    {url: '3-2.png', legenda: 'Vice-Prefeito', small: true}
+                    {url: '5-1.png', legenda: 'Prefeito', small: false},
+                    {url: '5-2.png', legenda: 'Vice-Prefeito', small: true}
                 ]
             },
             {
-                numero: 84,
-                nome: 'Zulano',
-                partido: 'QWERTY',
-                vice: 'Zul',
+                numero: '22',
+                nome: 'Sérgio Goulão',
+                partido: 'DEF',
+                vice: 'Denise Rodrigues',
                 fotos: [
-                    {url: '4-1.png', legenda: 'Prefeito', small: false},
-                    {url: '4-2.png', legenda: 'Vice-Prefeito', small: true}
+                    {url: '6-1.png', legenda: 'Prefeito', small: false},
+                    {url: '6-2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '33',
+                nome: 'Polina Cardim',
+                partido: 'GHI',
+                vice: 'Suely Bentes',
+                fotos: [
+                    {url: '7-1.png', legenda: 'Prefeito', small: false},
+                    {url: '7-2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '44',
+                nome: 'ZBrenda Sobral',
+                partido: 'ABC',
+                vice: 'Irina Milhães',
+                fotos: [
+                    {url: '8-1.png', legenda: 'Prefeito', small: false},
+                    {url: '8-2.png', legenda: 'Vice-Prefeito', small: true}
                 ]
             }
         ]
