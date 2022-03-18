5b3575256ae1c69121c2edbf8d2d27d2a3a05b05
Tiago Castilho <tiagocastilho_bm@hotmail.com>
2022-03-18 20:30:19 -0300

    Atualização de arquivos

--- Projeto 13 - Clima/script.js
+++ Projeto 13 - Clima/script.js
@@ -1,14 +1,15 @@
-document.querySelector('.busca').addEventListener('submit', async (event) => {
+document.querySelector('.busca').addEventListener('submit', async (event)=> {
     event.preventDefault();
 
     let input = document.querySelector('#searchInput').value;
-    
+
     if(input !== '') {
         clearInfo();
         showWarning('Carregando...');
 
-        let results = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=
-        ${encodeURI(input)}&units=metric&lang=pt_br&appid=d06cdb298fafc83c520d5ab677fc477e`);
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=ed89c8f2dddb9d9170e7a20f2479af8c&units=metric&lang=pt_br`;
+    
+        let results = await fetch(url);
         let json = await results.json();
 
         if(json.cod === 200) {
@@ -22,25 +23,24 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
             });
         } else {
             clearInfo();
-            showWarning('Não encontramos esta localização.');
+            showWarning('Localização não encontrada.');
         }
     } else {
         clearInfo();
     }
+
 });
 
-function showInfo(obj) {
+function showInfo(json) {
     showWarning('');
 
-    document.querySelector('.titulo').innerHTML = `${obj.name}, ${obj.country}`;
-    
-    document.querySelector('.tempInfo').innerHTML = `${obj.temp} <sup>ºC</sup>`;
-    document.querySelector('.ventoInfo').innerHTML = `${obj.windSpeed} <span>km/h</span>`;
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
 
-    document.querySelector('.temp img').setAttribute('src', 
-    `http://openweathermap.org/img/wn/${obj.tempIcon}@2x.png`);
-    
-    document.querySelector('.ventoPonto').style.transform = `rotate(${obj.windAngle-90}deg)`;
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
+
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
 
     document.querySelector('.resultado').style.display = 'block';
 }
